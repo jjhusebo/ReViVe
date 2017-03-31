858e4cb6f472100fd672a751d3f0013b87bf3e2e
CXu <cxu@live.no>
2017-04-01 01:50:12 +0200

    Play open door animation on trigger

---
+++ Assets/Resources/Scripts/OpenDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        Debug.Log("Script ran!");
+    }
+	
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collider");
+        if (other.tag == "Player")
+        {
+            animator.SetBool("open", true);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+    }
+}
