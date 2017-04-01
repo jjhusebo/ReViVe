52d3681ba7997bed337d6fe0bcdd43ffbe70ba82
CXu <cxu@live.no>
2017-04-01 17:42:11 +0200

    Contraption logic to show electron flow animation

---
+++ Assets/ElectronScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronScript : MonoBehaviour {
+
+    Animator animator;
+    public float offset;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        animator.Play("Electron", -1, offset);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
