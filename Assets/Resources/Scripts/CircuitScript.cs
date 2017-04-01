23569340dd6492dfd8cd51e00a84ddf1e67362e0
CXu <cxu@live.no>
2017-04-01 17:42:11 +0200

    Contraption logic to show electron flow animation

---
+++ Assets/Resources/Scripts/CircuitScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitScript : MonoBehaviour {
+    public Animator circuitAnimator;
+    public GameObject lightBulb;
+    public GameObject electronFlow;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+    public void turnOn()
+    {
+        electronFlow.GetComponent<ElectronFlowScript>().createElectrons();
+    }
+
+    public void turnOff()
+    {
+        electronFlow.GetComponent<ElectronFlowScript>().destroyElectrons();
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Resources/Scripts/MachineScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineScript : MonoBehaviour {
+
+    public GameObject circuit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Battery")
+        {
+            Debug.Log("Battery on trigger!");
+            circuit.GetComponent<CircuitScript>().turnOn();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Battery")
+        {
+            Debug.Log("Battery removed!");
+            circuit.GetComponent<CircuitScript>().turnOff();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
