4c6bb2870407cdc494c402492c1d6982269d06e7
CXu <cxu@live.no>
2017-04-01 18:03:07 +0200

    Puzzle works properly

---
+++ Assets/Resources/Scripts/LightScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+    public Material lightOff;
+    public Material lightOn;
+    public Light lightSource;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material = lightOff;
+        lightSource.enabled = false;
+    }
+	
+    public void turnOn()
+    {
+        GetComponent<Renderer>().material = lightOn;
+        lightSource.enabled = true;
+    }
+
+    public void turnOff()
+    {
+        GetComponent<Renderer>().material = lightOff;
+        lightSource.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
