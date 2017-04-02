6c3bea8426b137e107d1f4f637f37c6345715271
Jonas J. Husebø <jonasjhu@stud.ntnu.no>
2017-04-02 17:58:24 +0200

    Now able to pick up items

---
+++ Assets/Resources/Scripts/Item.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Item : MonoBehaviour {
+    public GameObject item;
+    public Sprite icon;
+    public bool canPickUp = true;
+    public Transform player;
+    public float gazeTime = 2f;
+
+    private float timer;
+    private bool gazedAt;
+
+    // Use this for initialization
+    void Start() {
+        player = GameObject.Find("PlayerWithMovement").transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (gazedAt) {
+            timer += Time.deltaTime;
+            if (timer >= gazeTime) {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0f;
+            }
+        }
+    }
+
+    public void PointerEnter() {
+        gazedAt = true;
+    }
+
+    public void PointerExit() {
+        gazedAt = false;
+    }
+
+    public void PointerDown() {
+
+        if (player.Find("Main Camera").FindChild("Inventory").childCount == 0) { 
+            if (canPickUp) {
+                transform.parent = player.Find("Main Camera").FindChild("Inventory").transform;
+                transform.GetComponent<Collider>().enabled = false;
+                transform.GetComponent<Rigidbody>().useGravity = false;
+                transform.SetPositionAndRotation(transform.parent.position, transform.parent.rotation);
+            }
+        } else {
+            return;
+        }
+    }
+}
