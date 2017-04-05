1a1197ddcf7c9671204a71fd64fe3f7b21ff308d
Tom <txkong@stud.ntnu.no>
2017-04-05 11:24:30 +0200

    Adds a restart button for easier prototyping

---
+++ Assets/Resources/Scripts/RestartScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class RestartScript : MonoBehaviour {
+	public string levelToLoad;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(CrossPlatformInputManager.GetButtonDown("Restart"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(levelToLoad);
+        }
+	}
+}
